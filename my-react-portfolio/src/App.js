d631cc5d8f1feea4e6f511b0188070483830d257
Nicholas Roberto <njr444@gmail.com>
2020-06-04 15:02:11 -0700

    add app routes for components and global styles.

--- my-react-portfolio/src/App.js
+++ my-react-portfolio/src/App.js
@@ -1,12 +1,22 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Link, Switch } from 'react-router-dom'
+import { GlobalStyle } from './global.styles'
+import Header from './components/Header';
+import './App.css'
 
-function App() {
+const App = () {
   return (
-    <div className="App">
-      
-    </div>
-  );
+    <Router>
+      <GlobalStyle />
+      <Header />
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/about' component={About} />
+        <Route exact path='/work' component={Work} />
+        <Route exact path='/about' component={Contact} />
+      </Switch>
+    </Router>
+  )
 }
 
-export default App;
+export default App
