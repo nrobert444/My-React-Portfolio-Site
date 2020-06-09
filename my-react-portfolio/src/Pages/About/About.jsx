d7b373e1e9ca769253b0ab8173b852bcf45dc65d
Nicholas Roberto <njr444@gmail.com>
2020-06-09 15:00:54 -0700

    moved logic outside of about page and fully into components.

--- my-react-portfolio/src/Pages/About/About.jsx
+++ my-react-portfolio/src/Pages/About/About.jsx
@@ -1,31 +1,15 @@
 import React from 'react'
 import HeaderSub from '../../components/HeaderSub/HeaderSub'
 import { Footer } from '../../components/Footer/Footer'
-import { Container } from '../../components/Container/Container'
 import { AboutInfo } from '../../components/AboutInfo/AboutInfo'
 import { AboutTech } from '../../components/AboutTech/AboutTech'
-import {
-  BottomLine,
-  SectionTitle,
-  Lead
-} from '../../components/Container/Container.style'
 
 export const About = () => {
   return (
     <div>
       <HeaderSub />
-      <section className='text-center py-3'>
-        <Container>
-          <SectionTitle></SectionTitle>
-          <BottomLine />
-          <Lead>Let me tell you a little about myself and what I do...</Lead>
-          <AboutInfo />
-        </Container>
-      </section>
+      <AboutInfo />
       <AboutTech />
-
-      {/* <!-- Section B: Technologies Figure out what to do here for icons--> */}
-
       <Footer />
     </div>
   )
--- my-react-portfolio/src/components/AboutInfo/AboutInfo.jsx
+++ my-react-portfolio/src/components/AboutInfo/AboutInfo.jsx
@@ -1,22 +1,35 @@
 import React from 'react'
 import { AboutInfoStyles } from './AboutInfo.styles'
+import { Container } from '../Container/Container'
+import {
+  BottomLine,
+  SectionTitle,
+  Lead
+} from '../../components/Container/Container.style'
 
 export const AboutInfo = () => {
   return (
     <div>
-      <AboutInfoStyles>
-        <img src='img/about-pic.jpg' alt='' className='bio-image' />
-        <div className='bio bg-light'>
-          <h4>Your Project Is In Safe Hands</h4>
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-            Perspiciatis nostrum, eaque velit ipsam facilis sed, quisquam
-            nesciunt unde nihil voluptatum commodi consequuntur molestiae?
-            Fugiat, perspiciatis reprehenderit. Architecto delectus vitae
-            deserunt!
-          </p>
-        </div>
-      </AboutInfoStyles>
+      <section className='text-center py-3'>
+        <Container>
+          <SectionTitle></SectionTitle>
+          <BottomLine />
+          <Lead>Let me tell you a little about myself and what I do...</Lead>
+          <AboutInfoStyles>
+            <img src='img/about-pic.jpg' alt='' className='bio-image' />
+            <div className='bio bg-light'>
+              <h4>Your Project Is In Safe Hands</h4>
+              <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Perspiciatis nostrum, eaque velit ipsam facilis sed, quisquam
+                nesciunt unde nihil voluptatum commodi consequuntur molestiae?
+                Fugiat, perspiciatis reprehenderit. Architecto delectus vitae
+                deserunt!
+              </p>
+            </div>
+          </AboutInfoStyles>
+        </Container>
+      </section>
     </div>
   )
 }
